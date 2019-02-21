d3e9d3161acf73ea1b6010a240fca721d55db9a9
AVoloshin <a.voloshin@tut.by>
2019-02-21 23:59:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Calc calc = new Calc();
-        calc.printCalc();
+        Patient patient = new Patient();
+        patient.readFile(3);
     }
 }
